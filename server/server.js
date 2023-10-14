84119c0382ba400d4e5dfe161fa3218c3b1f32d2
Tristenh <tristenhobba1@gmail.com>
2023-10-14 18:07:53 +1100

    updated server.js to integrate Apollo for graphQL support, imported required packages, create instance of Apollo server, added wildcard route launch server on /graphql route

--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+// Import the ApolloServer class
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express");
+const path = require("path");
 
-const app = express();
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// create a express app
+const app = express();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// create the Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  // start the Apollo server
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // launch Apollo server using the expressMiddleware on a specific route, you can call the route anything
+  app.use("/graphql", expressMiddleware(server));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === "production") {
+    // Serve static files from the client-side build directory
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    // Handle any other routes by serving the main index.html
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  // launch app server
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+// Call the async function to start the server
+startApolloServer();
