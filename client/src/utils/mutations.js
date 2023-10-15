114e9e0b045928112450d5758d1ea0b2e4a5ec0f
Tristenh <tristenhobba1@gmail.com>
2023-10-15 12:57:08 +1100

    Added mutations.js, LOGIN_USER, ADD_USER, SAVE_BOOK & REMOVE_BOOK executes login, addUser, saveBook and removeBook operations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from "@apollo/client";
+
+// execute login
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// execute addUser
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// execute saveBook
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $bookId: ID!
+    $image: String!
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+    ) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+// execute removeBook
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
